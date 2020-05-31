7dfed3fc6fd7fc88b472a418f018f57e40a7d844
Jakaria Hussain <jakariahussain95@gmail.com>
2020-06-01 01:18:08 +0600

    MY DOM CRUD JAVA

---
+++ app.js
@@ -0,0 +1,134 @@
+// let form = document.querySelector('#task-form')
+// let taskList = document.querySelector('.collection')
+// let TaskInput = document.querySelector('#TaskInput')
+
+// //Load All Events
+// loadEventListeners()
+
+// function loadEventListeners() {
+//     form.addEventListener('submit', addTask)
+// }
+
+// //Add Task
+// function addTask(e) {
+//     if (TaskInput.value === '') {
+//         alert('Please Add a Task')
+//     }
+//     let li = document.createElement('li')
+//     li.classList.add('list-group-item')
+//     li.appendChild(document.createTextNode(TaskInput.value))
+
+//     let link = document.createElement('a')
+//     link.className = 'close'
+//     link.innerHTML = '<i class="fas fa-minus"></i>'
+//     li.appendChild(link)
+
+//     taskList.appendChild(li)
+//     TaskInput.value = ''
+//     e.preventDefault()
+// }
+
+// //Define UI
+// let taskForm = document.querySelector('#task-form')
+// let TaskInput = document.querySelector('#TaskInput')
+// let taskCollection = document.querySelector('.collection')
+
+// //Load all Events
+// loadEventListeners()
+
+// function loadEventListeners() {
+//     taskForm.addEventListener('submit', addTask)
+// }
+// //Add Task
+// function addTask(e) {
+//     if (TaskInput.value === '') {
+//         alert('Add A Task')
+//     }
+//     let li = document.createElement('li')
+//     li.className = 'list-group-item'
+//     li.appendChild(document.createTextNode(TaskInput.value))
+
+//     let link = document.createElement('a')
+//     link.className = 'close'
+//     link.innerHTML = '<i class="fas fa-minus"></i>'
+//     li.appendChild(link)
+
+//     taskCollection.appendChild(li)
+//     TaskInput.value = ''
+//     e.preventDefault()
+// }
+
+
+//Defne UI
+let taskForm = document.querySelector('#task-form')
+let TaskInput = document.querySelector('#TaskInput')
+let taskCollection = document.querySelector('.collection')
+let clear = document.querySelector('.clears')
+let filter = document.querySelector('#filter')
+
+//Load All Events
+loadEventListeners()
+
+
+//add Task
+function loadEventListeners() {
+    //Add task
+    taskForm.addEventListener('submit', addTask)
+
+    //Remove Task
+    taskCollection.addEventListener('click', removes)
+
+    //clears
+    clear.addEventListener('click', clears)
+
+    //filter
+    filter.addEventListener('keyup', filterTask)
+}
+
+function addTask(e) {
+    if (TaskInput.value === '') {
+        alert('Please Add Value')
+    }
+
+    let li = document.createElement('li')
+    li.className = 'list-group-item'
+    li.appendChild(document.createTextNode(TaskInput.value))
+
+    let link = document.createElement('a')
+    link.className = 'close'
+    link.innerHTML = '<i class="fas fa-minus"></i>'
+    li.appendChild(link)
+
+    taskCollection.appendChild(li)
+    TaskInput.value = ''
+    e.preventDefault()
+}
+
+//Remove Task
+function removes(e) {
+    if (e.target.parentElement.classList.contains('close'))
+        if (confirm('Are You sure')) {
+            e.target.parentElement.parentElement.remove()
+        }
+}
+
+//clears Task
+
+function clears(e) {
+    taskCollection.innerHTML = ''
+    e.preventDefault()
+}
+
+//filters Item
+
+function filterTask(e) {
+    let text = e.target.value.toLowerCase()
+    document.querySelectorAll('.list-group-item').forEach(function(task) {
+        const item = task.firstChild.textContent;
+        if (item.toLowerCase().indexOf(text) != -1) {
+            task.style.display = 'block'
+        } else {
+            task.style.display = 'none'
+        }
+    })
+}
